11b7681bd70e46d6999deca6e9aa34f565f96d50
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2022-12-03 13:17:52 -0500

    replaced and added assets

--- Unknown/Assets/Scripts/Doors/DoorTrigger.cs
+++ Unknown/Assets/Scripts/Doors/DoorTrigger.cs
@@ -6,8 +6,10 @@ public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] List<Door> doors;
 
+    public bool lockState = false;
+
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "Player"){
+        if(other.gameObject.tag == "Player" && !lockState){
             foreach(Door door in doors){
                 if(!door.isOpen){
                     door.Open(other.transform.position);
@@ -17,11 +19,27 @@ public class DoorTrigger : MonoBehaviour
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.gameObject.tag == "Player"){
+        if(other.gameObject.tag == "Player" && !lockState){
             foreach(Door door in doors){
                 if(door.isOpen){
                     door.Close();
                 }
+            }
+        }
+    }
+
+    public void ForceOpen() {
+        foreach(Door door in doors){
+            if(!door.isOpen && !lockState){
+                door.Open(other.transform.position);
+            }
+        }
+    }
+
+    public void ForceClose() {
+        foreach(Door door in doors){
+            if(door.isOpen && !lockState){
+                door.Close();
             }
         }
     }
